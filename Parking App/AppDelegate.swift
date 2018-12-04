70df2899dae132146e43289337763dc0a5b08454
Sunny Ahmed <sunny.ahmed7575@gmail.com>
2018-12-04 17:49:55 +0500

    no message

--- Parking App/AppDelegate.swift
+++ Parking App/AppDelegate.swift
@@ -11,6 +11,9 @@ import Firebase
 import GoogleMaps
 import GooglePlaces
 import Localize
+import UserNotifications
+import FirebaseInstanceID
+import FirebaseMessaging
 
 import IQKeyboardManagerSwift
 
@@ -31,6 +34,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var kuserDef = UserDefaults.standard
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            Messaging.messaging().delegate = self
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+       
 //
 //        let localize = Localize.shared
 //        // Set your localize provider.
@@ -98,3 +120,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate : UNUserNotificationCenterDelegate, MessagingDelegate{
+    
+    func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
+    
+    
+}
